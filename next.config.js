5cad1a554cf1d8bae177fceac8c7b9d82057721f
Hersh Thakur <hersh.thakur@gmail.com>
2023-10-16 16:51:15 +0100

    sorted out the image issue and now I have a thread card

--- next.config.js
+++ next.config.js
@@ -5,33 +5,33 @@ const nextConfig = {
     serverComponentsExternalPackages: ["mongoose"],
   },
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   images: {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "images.clerk.dev",
-        },
-        {
-          protocol: "https",
-          hostname: "uploadthing.com",
-        },
-        {
-          protocol: "https",
-          hostname: "placehold.co",
-        },
-        {
-          protocol: "https",
-          hostname: "utfs.io",
-        },
-      ],
-    },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io"
+      }
+    ],
   },
 };
 
