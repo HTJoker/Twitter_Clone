7abe1692c724612794261db7549ce7b8424dbe5c
Hersh Thakur <hersh.thakur@gmail.com>
2023-10-18 13:55:02 +0100

    added profile page and tabs for different sections of profile

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -3,5 +3,6 @@ import BottomBar from "./BottomBar";
 import LeftSideBar from "./LeftSideBar";
 import RightSideBar from "./RightSideBar";
 import ProfileHeader from "./ProfileHeader";
+import ThreadsTab from "./ThreadsTab";
 
-export { TopBar, BottomBar, LeftSideBar, RightSideBar, ProfileHeader };
+export { TopBar, BottomBar, LeftSideBar, RightSideBar, ProfileHeader, ThreadsTab };
